000822bff96d5ca0107f7b8723a7affbdfa9116d
It's Sappy <57357216+mastrmind-dev@users.noreply.github.com>
2020-05-25 15:57:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-document.addEventListner(DOMContentLoaded, () => {
-  
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#Score')
+  const startBtn = document.querySelector('#start-button')
+  const width = 10
+
+
 })
